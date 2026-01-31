f7af1dbaca51db06d32ecadab579e4d1125bff30
ujju16 <jul.darrouza@gmail.com>
2026-01-31 09:50:29 +0100

    chore: add classComponent structre

--- src/Components/EventPlanner.jsx
+++ src/Components/EventPlanner.jsx
@@ -4,10 +4,31 @@ import './EventPlanner.css'; // Import CSS file for styling
 const EventPlanner = () => {
     return (
         <div className="event-planner-container">
-            <header>
-                <h1>Welcome to Event Planner</h1>
-            </header>
-        </div>
+    {/* Page Header */}
+    <header>
+        <h1>Welcome to Event Planner</h1>
+    </header>
+    {/* Section for describing the purpose or overview of the app */}
+    <section className="description">
+        {/* Description content goes here */}
+    </section>
+    {/* Section to list or categorize different types of events */}
+    <section className="events_categories">
+        {/* Event categories content goes here */}
+    </section>
+    {/* Section to highlight app features or functionalities */}
+    <section className="features">
+        {/* Features content goes here */}
+    </section>
+    {/* Section to showcase user reviews or testimonials */}
+    <section className="testimonials">
+        {/* Testimonials content goes here */}
+    </section>
+    {/* Section to provide contact information or a contact form */}
+    <section className="contact">
+        {/* Contact content goes here */}
+    </section>
+</div>
     );
 };
 
